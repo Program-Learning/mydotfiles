a862e6686ac2dd9f9508e8919694ec56d5a28f95
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-06-29 19:11:23 +0800

    SnapShot

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias optirun='__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only'
